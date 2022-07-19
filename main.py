da3f4893b36915c007b66f0f5cdf6815e2f705a8
Naveen Dharni <naveendharni@Naveens-MacBook-Air.local>
2022-07-19 19:45:35 -0400

    modified main file and rename tempdb

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-print('Hello World!!')
+# import dbOperation
 
+noExit = True
+
+while noExit:
+    print("\nChoose an option for login \n 1-Admin \n 2-Customer \n 3-Exit")
+    try:
+        choice = int(input("Select an option: "))
+    except ValueError:
+        print('\nWrong selection!! Try again.')
+        continue
+
+    if choice == 3:
+        print("Exit")
+        noExit = False
+    else:
+        loginId = input("Enter login Id: ").lower()
+        password = input("Enter password: ")
+        # user = dbOperation.checkLoginId(loginId)
+
+        if not user:  # User not found
+            print('User not found!')
+        elif choice == 1:  # admin(1)
+            print("Show admin options")  # remove this after adding code for admin
+        #   Create new file, import and call function here to display the admin options
+        else:  # customer(2)
+            print("Show customer options")  # remove this after adding code for customer
+        #   Create new file, import and call function here to display the customer options
