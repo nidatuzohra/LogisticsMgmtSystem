1c0210f2952def546438f0bf650e613ab8b9e0df
Naveen Dharni <naveendharni@Naveens-MacBook-Air.local>
2022-07-22 18:36:17 -0400

    remove tempDB and added minor tweaks

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import adminMenu1
 noExit = True
 
 while noExit:
-    print("\nChoose an option for logins \n 1-Admin \n 2-Customer \n 3-Exit")
+    print("\nChoose an option for login \n 1-Admin \n 2-Customer \n 3-Exit")
     try:
         choice = int(input("Select an option: "))
     except ValueError:
@@ -20,14 +20,12 @@ while noExit:
         # user = dbOperation.checkLoginId(loginId)
         # user = adminOperations.checkLoginId(loginId) # generic function can go in a different py file?
 
-        user = True
+        user = True #to pass the validation
 
         if not user:  # User not found
             print('User not found!')
         elif choice == 1:  # admin(1)
-            print("Show admin options")  # remove this after adding code for admin
             adminMenu1.adminMenu()
-        #   Create new file, import and call function here to display the admin options
         else:  # customer(2)
             print("Show customer options")  # remove this after adding code for customer
         #   Create new file, import and call function here to display the customer options
